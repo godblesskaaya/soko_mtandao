d849ba3629845c76444de489feb8630e27aeb705
godblesskaaya <155067647+godblesskaaya@users.noreply.github.com>
2026-08-13 16:12:50 +0200

    ci: align android plugin compile sdk

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -5,6 +5,14 @@ allprojects {
     }
 }
 
+subprojects {
+    pluginManager.withPlugin("com.android.library") {
+        extensions.configure<com.android.build.api.dsl.LibraryExtension>("android") {
+            compileSdk = 36
+        }
+    }
+}
+
 val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
 rootProject.layout.buildDirectory.value(newBuildDir)
 
